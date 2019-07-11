99e93280dd82224b05429e8958bee5314f1ae5a4
= <=>
2019-07-11 12:29:05 +0700

    1.3 The MyPoint

---
+++ src/Exercises_on_Classes_and_Instances/Exercises_1_3_The_MyPoint_class/TestMyPoint.java
@@ -0,0 +1,16 @@
+package Exercises_on_Classes_and_Instances.Exercises_1_3_The_MyPoint_class;
+
+public class TestMyPoint {
+    public static void main(String[] args) {
+        MyPoint p1 = new MyPoint(3, 0);
+        MyPoint p2 = new MyPoint(0, 4);
+        System.out.println(p1.distance(p2));
+        System.out.println(p1.distance(5, 6));
+
+
+        MyPoint[] points = new MyPoint[10];
+        for (int i = 0; i < points.length; i++) {
+            points[i] = new MyPoint(i + 1, i + 1);
+        }
+    }
+}
